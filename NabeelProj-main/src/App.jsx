a97ba42b04947dd0da0d38b32566095917732991
Arsalanbashir831 <arsalanbashir831@gmail.com>
2022-08-17 12:10:30 +0500

    comit

--- NabeelProj-main/src/App.jsx
+++ NabeelProj-main/src/App.jsx
@@ -1,15 +1,18 @@
 import React, { useState } from 'react'
 import Home from './Pages/Home'
+import Bg from "./assets/background.webp"
 import './index.css'
 const App = () => {
     const [darkMode, setDarkMode] = useState(true)
     return (
         <div className={`${darkMode ? 'dark' : ''} ds`}>
-            <div className={`${darkMode ? 'bg-gradient-to-r from-cyan-700 to-blue-700' : 'bg-gradient-to-r from-cyan-500 to-blue-500'} `}>
+            <div  >
                 <Home darkMode={darkMode} setDarkMode={setDarkMode} />
             </div>
         </div>
     )
 }
+//for gradient background
+// className={`${darkMode ? 'bg-gradient-to-r from-cyan-700 to-blue-700' : 'bg-gradient-to-r from-cyan-500 to-blue-500'} `}
 
 export default App
